6fd202fe5fe916da9add9522aec40c8e3186e1c8
hadifayazi <hadifayazi4@gmail.com>
2023-06-26 01:01:30 +0200

    upadating category items

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
+
 const CategoryItem = ({ category }) => {
   return (
-    <div className="relative w-full ">
-      <div className="absolute p-2 ">
-        <img src={category.img} alt="productImage" className="cover" />
-      </div>
-      <div className="flex justify-center ">
-        <h1 className="absolute text-white font-bold m-4">{category.title}</h1>
-        <button className="absolute text-white  bg-black/40 hover:bg-custRed my-[50%] px-2 py-1 rounded-xl ">
-          Shop now
-        </button>
+    <div className="flax-1 w-full m-1 h-5/6 relative">
+      <img src={category.img} alt="productImage" className="object-cover " />
+      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center flex-col ">
+        <h1 className="text-white font-bold">{category.title}</h1>
+        <Link
+          to={`/category/${category.title}`}
+          type="button"
+          className="bg-black/40 text-white px-2 py-1 rounded-md mt-4 hover:bg-pink-700"
+        >
+          See products
+        </Link>
       </div>
     </div>
   );
