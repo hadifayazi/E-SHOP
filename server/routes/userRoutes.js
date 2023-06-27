3a8a3f6c9946d35eee915cf85c4c57926d2a3f1e
hadifayazi <hadifayazi4@gmail.com>
2023-06-27 02:55:06 +0200

    adding more user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { verifyEmail, login } from "../controllers/authController.js";
+import {
+  verifyEmail,
+  login,
+  forgotPassword,
+  resetPassword,
+  logout,
+} from "../controllers/authController.js";
 import isAuthenticated from "../utils/isAuthenticated.js";
 import { getUser } from "../controllers/userController.js";
 
@@ -7,6 +13,10 @@ const router = express.Router();
 
 router.post("/verify-email", verifyEmail);
 router.post("/login", login);
+router.get("/logout", isAuthenticated, logout);
+router.post("/forgot-password", forgotPassword);
+router.patch("/reset-password", resetPassword);
+
 router.get("/get-user", isAuthenticated, getUser);
 
 export default router;
