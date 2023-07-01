5835428b280020186e27793f4a6a7ce355e2600a
hadifayazi <hadifayazi4@gmail.com>
2023-07-01 12:35:21 +0200

    stripe controller update backend

--- server/controllers/stripeController.js
+++ server/controllers/stripeController.js
@@ -1,34 +1,41 @@
 import Stripe from "stripe";
-
-const stripe = new Stripe(process.env.Stripe_KEY);
-
+import dotenv from "dotenv";
+dotenv.config();
+const stripe = Stripe(process.env.STRIPE_KEY);
 export const payment = async (req, res) => {
   const { email, amount } = req.body;
-  await stripe.customers
-    .create({
+  try {
+    const customer = await stripe.customers.create({
       email: email,
-    })
-    .then(async (customer) => {
-      // have access to the customer object
-      return stripe.invoiceItems
-        .create({
-          customer: customer.id, // set the customer id
-          amount: amount, // 25
-          currency: "euro", // set the currency
-        })
-        .then((invoiceItem) => {
-          return stripe.invoices.create({
-            collection_method: "send_invoice",
-            customer: invoiceItem.customer,
-          });
-        })
-        .then((invoice) => {
-          res.status(200).json(invoice);
-          // New invoice created on a new customer
-        })
-        .catch((err) => {
-          res.status(500).json({ message: err.message });
-          // Deal with an error
-        });
     });
+
+    const invoiceItem = await stripe.invoiceItems.create({
+      customer: customer.id,
+      amount: amount,
+      currency: "EUR",
+    });
+
+    const invoice = await stripe.invoices.create({
+      collection_method: "send_invoice",
+      customer: invoiceItem.customer,
+      due_date: Math.floor(Date.now() / 1000) + 7 * 24 * 60 * 60, // Set due date to 7 days from now
+    });
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      customer: customer.id,
+      amount: amount,
+      currency: "EUR",
+      payment_method_types: ["card"],
+      description: "Payment for invoice " + invoice.id,
+      metadata: { invoice_id: invoice.id },
+    });
+    console.log("INVOICE ITEM:", invoiceItem);
+    console.log("****************************");
+    console.log("Invoice:", invoice);
+    console.log("****************************");
+    console.log("paymentIntent:", paymentIntent);
+    res.status(200).json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
