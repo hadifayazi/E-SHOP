54782dce440ef5deb69c4cf975e9f69044e8e0a8
hadifayazi <hadifayazi4@gmail.com>
2023-07-01 12:34:43 +0200

    add stripe api to the store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import userApi from "../features/api/userApi";
 import productApi from "../features/api/productApi";
+import stripeApi from "../features/api/stripeApi";
 import { authReducer } from "../features/user/authSlice";
 
 const store = configureStore({
@@ -9,9 +10,14 @@ const store = configureStore({
     auth: authReducer,
     [userApi.reducerPath]: userApi.reducer,
     [productApi.reducerPath]: productApi.reducer,
+    [stripeApi.reducerPath]: stripeApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware, productApi.middleware),
+    getDefaultMiddleware().concat(
+      userApi.middleware,
+      productApi.middleware,
+      stripeApi.middleware
+    ),
 });
 setupListeners(store.dispatch);
 
