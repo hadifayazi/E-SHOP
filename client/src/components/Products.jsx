c34a13c9f7e37f9fc222308942c1e864412257ff
hadifayazi <hadifayazi4@gmail.com>
2023-07-02 21:51:17 +0200

    implementing filter for products

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -2,8 +2,9 @@ import { styled } from "styled-components";
 import ProductItems from "./ProductItems";
 import { mobile } from "../utils/responsive";
 import { useGetAllProductsQuery } from "../features/api/productApi";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import { useLocation } from "react-router-dom";
 
 const Container = styled.div`
   ${mobile({
@@ -16,25 +17,77 @@ const Container = styled.div`
 `;
 
 const Products = () => {
-  const { data, isSuccess, isError, error } = useGetAllProductsQuery();
+  const location = useLocation();
+  const category = location.pathname.split("/")[2];
+  const [filters, setFilters] = useState({});
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const { data, isSuccess, isError, error } = useGetAllProductsQuery(category);
 
   useEffect(() => {
     if (isSuccess) {
-      console.log(data);
+      setFilteredProducts(
+        data?.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
     }
     if (isError) {
       toast.error(error.data?.message);
     }
-  }, [isSuccess, data, isError, error]);
+  }, [isSuccess, data, isError, error, filters]);
 
-  const renderedProductItems = data?.map((product) => {
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setFilters({
+      ...filters,
+      [e.target.name]: value,
+    });
+  };
+
+  const renderedProductItems = filteredProducts?.map((product) => {
     return <ProductItems key={product._id} product={product} />;
   });
+
   return (
     <>
-      <h1 className="ml-2 sm:ml-[35%] mt-12 mb-5 text-lg  sm:text-3xl">
-        Most popular products:
-      </h1>
+      <div className="flex justify-between mx-8 m-6 w-[95%] ">
+        <div>
+          <label>Filter by color:</label>
+          <select
+            name="color"
+            className="border border-black rounded-lg ml-1"
+            onChange={handleChange}
+          >
+            <option disabled></option>
+            <option>white</option>
+            <option>black</option>
+            <option>blue</option>
+            <option>yellow</option>
+            <option>red</option>
+            <option>other</option>
+          </select>
+        </div>
+        <div>
+          <label>Filter by Size:</label>
+          <select
+            name="size"
+            className="border border-black rounded-lg ml-1"
+            onChange={handleChange}
+          >
+            <option value="" disabled>
+              -- Select --
+            </option>
+            <option>XS</option>
+            <option>S</option>
+            <option>L</option>
+            <option>XL</option>
+            <option>XXL</option>
+          </select>
+        </div>
+      </div>
+
       <Container className="flex flex-wrap justify-between items-center py-8 bg-slate-200 ">
         {renderedProductItems}
       </Container>
